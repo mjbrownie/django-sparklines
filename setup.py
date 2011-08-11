15178ed3e80904ffdaa7a918c6a84f0de0f7d123
Michael Brown <mjbrownie@gmail.com>
2011-08-11 16:10:24 +1000

    added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(name='django-sparklines',
+        version='0.1',
+        description='Simple tag abstraction for jquery sparklines',
+        #long_description=readme,
+        author="Michael Brown",
+        author_email="mjbrownie@gmail.com",
+        packages=find_packages(),
+        zip_safe=False,
+        )

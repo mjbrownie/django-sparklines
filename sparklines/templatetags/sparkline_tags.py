d30d85e5e2154d6a5a80d02869ef67aaba4ec1d4
Michael Brown <mjbrownie@gmail.com>
2011-08-16 08:58:45 +1000

    added counter for unique span tag ids

--- sparklines/templatetags/sparkline_tags.py
+++ sparklines/templatetags/sparkline_tags.py
@@ -14,12 +14,17 @@ except:
     STATIC_URL = settings.MEDIA_URL
 
 def _queryset_sparkline(queryset,date_field,options={},*args,**kwargs):
+    _queryset_sparkline.counter += 1
+    sparkline_type = 'queryset'
+    counter = _queryset_sparkline.counter
     module_name = queryset.model._meta.module_name
     options = mark_safe( simplejson.dumps(options) )
     sparkline_list =  get_object_date_count_list(queryset,
             date_field, *args, **kwargs)
 
     return locals()
+
+_queryset_sparkline.counter = 0
 
 @register.inclusion_tag('sparklines/sparkline.html')
 def queryset_sparkline(queryset,date_field,
@@ -35,7 +40,12 @@ def queryset_sparkline(queryset,date_field,
 @register.inclusion_tag('sparklines/sparkline.html')
 def sparkline(sparkline_list,options = {'type':'bar'}):
     options = mark_safe( simplejson.dumps(options) )
+    sparkline_type = 'list'
+    sparkline.counter += 1
+    counter = sparkline.counter
     return locals()
+
+sparkline.counter = 0
 
 @register.inclusion_tag('sparklines/sparklines_script_tag.html')
 def sparklines_script_tag():
